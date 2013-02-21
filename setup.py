8e590a13e53883a7112b1c4a3e827f1667f336ef
Julien Rebetez <julien@fhtagn.net>
2013-02-21 22:11:15 +0100

    Fix problem with numpy include path

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(name='python-pcl',
       description='pcl wrapper',
@@ -12,9 +13,9 @@ setup(name='python-pcl',
       ext_modules=[Extension(
                    "pcl",
                    ["pcl.pyx", "minipcl.cpp"],
-                    include_dirs=["/usr/include/pcl-1.5", "/usr/include/eigen3/"],
-                    libraries=["pcl_segmentation", "pcl_io", "OpenNI",
-                               "usb-1.0", "pcl_filters", "pcl_sample_consensus",
+                    include_dirs=["/usr/include/pcl-1.5", "/usr/include/eigen3/",
+                                  numpy.get_include()],
+                    libraries=["pcl_segmentation", "pcl_io", "pcl_filters", "pcl_sample_consensus",
                                "pcl_features", "pcl_surface", "pcl_search", "pcl_kdtree", "pcl_octree",
                                "flann_cpp", "pcl_common"],
                     language="c++")],
